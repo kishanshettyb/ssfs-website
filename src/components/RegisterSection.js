4cb0535690b0fda78bda3511544b73d3c660f52e
Kishan B <kishanqr@gmail.com>
2020-12-30 21:27:50 +0530

    add register section

--- src/components/RegisterSection.js
+++ src/components/RegisterSection.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 export default function RegisterSection() {
-	return <div></div>;
+	return (
+		<section className="register-section">
+			<Container>
+				<Row>
+					<Col className="text-center">
+						<h2 className="title">Register As A Professional in minutes.</h2>
+						<p>Get Your SSFS Acount Today!</p>
+						<Button size="lg" variant="dark">
+							Get Your Account
+						</Button>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
 }
